5313051342931cb199fd984c2e99a35b32fb5d45
Vigen Mkrtchyan <vigen.mkrtychyan7@gmail.com>
2025-10-04 13:27:19 +0400

    fix gitignore

---
+++ main.py
@@ -0,0 +1,20 @@
+import os
+from src.data_preprocessing import run_preprocessing
+from src.model_training import run_model_training
+
+def main():
+    os.makedirs('data/processed', exist_ok=True)
+    os.makedirs('submissions', exist_ok=True)
+
+    print("--- Starting Data Preprocessing ---")
+    run_preprocessing()
+    print("--- Preprocessing Complete ---")
+
+    print("\n--- Starting Model Training ---")
+    run_model_training()
+    print("--- Model Training Complete and Comparison Plotted ---")
+
+    print("\n--- Starting Model Evaluation ---")
+
+if __name__ == "__main__":
+    main()
